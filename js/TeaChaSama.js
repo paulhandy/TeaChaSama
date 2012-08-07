57a671f68ed403d083a0366b24f09d05052d2c9d
paulh <paulh@mint>
2012-08-06 18:41:21 -0600

    movingtoparagraph

--- js/TeaChaSama.js
+++ js/TeaChaSama.js
@@ -218,10 +218,10 @@ function Chapter(){
         //this.audio.controls.setAttribute('src', this.audio.filename);
         this.audio.controls.setAttribute('id', 'lessonAudio');
         this.audio.controls.setAttribute('controls', 'controls');
-        var alternate = '<object width="320" height="240" type="application/x-shockwave-flash" data="johndyer-mediaelement-b090320/build/flashmediaelement.swf"><param name="audio" value="flashmediaelement.swf" /><param name="flashvars" value="controls=true&file=../../'+this.audio.filename+'" /></object>';
+        //var alternate = '<object width="320" height="240" type="application/x-shockwave-flash" data="johndyer-mediaelement-b090320/build/flashmediaelement.swf"><param name="audio" value="flashmediaelement.swf" /><param name="flashvars" value="controls=true&file=../../'+this.audio.filename+'" /></object>';
         
         mediaelement.appendChild(this.audio.controls);
-        (this.audio.controls).innerHTML = alternate;
+        //(this.audio.controls).innerHTML = alternate;
         //this.audio.controls.load();
         $('#lessonAudio').mediaelementplayer({
             success: function(mediaElement, domObject){
